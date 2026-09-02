58b18b31991975d5b284ed21b445955b10283f3d
sina <163084137+sinavm@users.noreply.github.com>
2026-09-02 18:41:37 +0330

    fix: always write mix and tuic/hy2 xray files

--- sort.php
+++ sort.php
@@ -1,44 +1,46 @@
 <?php
-// Enable error reporting
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ERROR | E_PARSE);
 
-// Include the functions file
 require "functions.php";
 
-// Read the config.txt file and split it into an array by newline
+@mkdir("subscriptions/xray/normal", 0777, true);
+@mkdir("subscriptions/xray/base64", 0777, true);
+
 $configsArray = explode("\n", file_get_contents("config.txt"));
+$sortArray = [
+    "mix" => [],
+    "vmess" => [],
+    "vless" => [],
+    "reality" => [],
+    "trojan" => [],
+    "ss" => [],
+    "tuic" => [],
+    "hy2" => [],
+];
 
-// Initialize an empty array to hold the sorted configurations
-$sortArray = [];
-
-// Loop through each configuration in the configsArray
 foreach ($configsArray as $config) {
-    // Detect the type of the configuration
+    $config = trim($config);
+    if ($config === "") {
+        continue;
+    }
+    $decoded = urldecode($config);
     $configType = detect_type($config);
-    // Add the configuration to the corresponding array in sortArray
-    $sortArray[$configType][] = urldecode($config);
-    // If the configuration is of type "vless" and is a reality, add it to the "reality" array
+    if (!$configType) {
+        continue;
+    }
+    $sortArray["mix"][] = $decoded;
+    $sortArray[$configType][] = $decoded;
     if ($configType === "vless" && is_reality($config)) {
-        $sortArray["reality"][] = urldecode($config);
+        $sortArray["reality"][] = $decoded;
     }
 }
 
-// Loop through each type of configuration in sortArray
 foreach ($sortArray as $type => $sort) {
-    // If the type is not empty
-    if ($type !== "") {
-        // Join the configurations into a string, encode it to base64, and write it to a file
-        $tempConfigs = hiddifyHeader("SiNAVM | " . strtoupper($type)) . implode("\n", $sort);
-        $base64TempConfigs = base64_encode($tempConfigs);
-        file_put_contents("subscriptions/xray/normal/" . $type, $tempConfigs);
-        file_put_contents(
-            "subscriptions/xray/base64/" . $type,
-            $base64TempConfigs
-        );
-    }
+    $tempConfigs = hiddifyHeader("SiNAVM | " . strtoupper($type)) . implode("\n", $sort);
+    file_put_contents("subscriptions/xray/normal/" . $type, $tempConfigs);
+    file_put_contents("subscriptions/xray/base64/" . $type, base64_encode($tempConfigs));
 }
 
-// Print "done!" to the console
 echo "Sorting Done!";
